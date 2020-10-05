cfce607fdae9bde5308fac94497d18301f597b31
nowell <jorged104@gmail.com>
2020-10-05 13:56:41 -0600

    update

--- API/routes/usuario_route.js
+++ API/routes/usuario_route.js
@@ -4,10 +4,10 @@ import usuarioController from '../controllers/usuario_controller';
 
 const router = routerx();
 
-router.post('/add',usuarioController.add);
-router.get('/query',usuarioController.query);
-router.get('/list',usuarioController.list);
-router.put('/activate',usuarioController.activate);
-router.put('/deactivate',usuarioController.deactivate);
+//router.post('/add',usuarioController.add);
+//router.get('/query',usuarioController.query);
+//router.get('/list',usuarioController.list);
+//router.put('/activate',usuarioController.activate);
+//router.put('/deactivate',usuarioController.deactivate);
 router.post('/login', usuarioController.login);
 export default router;
